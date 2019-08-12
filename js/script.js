9b015659d46b958fd59145dcc0566417b4c1c4f1
Klaude Dungca <klaude.dungca@gmail.com>
2019-08-12 15:36:47 -0600

    added new img, started header & navbar

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//Get the current year
+$('#year').text(new Date().getFullYear());
+
+//slider configuration
+$('.carousel').carousel({
+    interval: 6000,
+    pause: 'hover'
+
+});
+
+// Video Play - from stackOverflow
+$(function () {
+    // Auto play modal video
+    $(".video").click(function () {
+        var theModal = $(this).data("target"),
+            videoSRC = $(this).attr("data-video"),
+            videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', videoSRC);
+        });
+    });
+});
